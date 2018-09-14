ce0d736a89ffca9091c1c6a70a53df84978c0715
정승욱 <wjdtmddnr24@naver.com>
2018-09-15 07:36:57 +0900

    workout, mypage페이지

---
+++ app/src/main/java/com/example/baehyeonbin/neverending_nightmare/WorkOutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.baehyeonbin.neverending_nightmare
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkOutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work_out)
+    }
+}
