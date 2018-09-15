eff8a6b614460f0a7361615c224455c8a7a49d9f
PPPosik <eoanswkgksk@gmail.com>
2018-09-15 16:02:08 +0900

    send receive page

--- app/src/main/java/com/example/baehyeonbin/neverending_nightmare/MoneyBackActivity.kt
+++ app/src/main/java/com/example/baehyeonbin/neverending_nightmare/MoneyBackActivity.kt
@@ -23,30 +23,30 @@ class MoneyBackActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.acrivity_moneyback)
-        var amount = intent.getIntExtra("amount", 0)
-        var wallet = intent.getStringExtra("wallet")
-        back_edittext.setText(intent.getIntExtra("amount", 0).toString())
-        back_button.setOnClickListener {
-            var roomService = RetrofitUtil.retrofit.create(RoomService::class.java)
-            var to = SharedPreferenceUtil.getPreference(applicationContext, "server_wallet")!!
-            var call = roomService.receiveCoin(ReceiveRequest(to, amount.toDouble(), wallet))
-            call.enqueue(object : Callback<ReceiveResponse> {
-                override fun onFailure(call: Call<ReceiveResponse>, t: Throwable) {
-                    Log.e(TAG, t.toString())
-                }
-
-                override fun onResponse(call: Call<ReceiveResponse>, response: Response<ReceiveResponse>) {
-                    if(response.body() != null) {
-                        when(response.code()) {
-                            200 -> {
-                                toast("성공적으로 받았습니다!")
-                                finish()
-                            }
-                        }
-                    }
-                }
-
-            })
-        }
+//        var amount = intent.getIntExtra("amount", 0)
+//        var wallet = intent.getStringExtra("wallet")
+//        back_edittext.setText(intent.getIntExtra("amount", 0).toString())
+//        back_button.setOnClickListener {
+//            var roomService = RetrofitUtil.retrofit.create(RoomService::class.java)
+//            var to = SharedPreferenceUtil.getPreference(applicationContext, "server_wallet")!!
+//            var call = roomService.receiveCoin(ReceiveRequest(to, amount.toDouble(), wallet))
+//            call.enqueue(object : Callback<ReceiveResponse> {
+//                override fun onFailure(call: Call<ReceiveResponse>, t: Throwable) {
+//                    Log.e(TAG, t.toString())
+//                }
+//
+//                override fun onResponse(call: Call<ReceiveResponse>, response: Response<ReceiveResponse>) {
+//                    if(response.body() != null) {
+//                        when(response.code()) {
+//                            200 -> {
+//                                toast("성공적으로 받았습니다!")
+//                                finish()
+//                            }
+//                        }
+//                    }
+//                }
+//
+//            })
+//        }
     }
 }
