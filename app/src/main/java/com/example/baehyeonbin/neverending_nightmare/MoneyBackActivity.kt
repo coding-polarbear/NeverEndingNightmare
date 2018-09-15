570f2271bb8e694b0c7bcdb171e44bd2b55f346f
PPPosik <eoanswkgksk@gmail.com>
2018-09-15 13:38:12 +0900

    button

--- app/src/main/java/com/example/baehyeonbin/neverending_nightmare/MoneyBackActivity.kt
+++ app/src/main/java/com/example/baehyeonbin/neverending_nightmare/MoneyBackActivity.kt
@@ -3,12 +3,15 @@ package com.example.baehyeonbin.neverending_nightmare
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.acrivity_moneyback.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MoneyBackActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.acrivity_moneyback)
 
-        back_edittext.setText("BACK")
+        back_button.setOnClickListener {
+            back_edittext.setText("BACK")
+        }
     }
 }
